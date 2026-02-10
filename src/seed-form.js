fdc77d4f9a8790f2bfcc12f400cc319ac43abb7e
Anton Kurhuzenkau <kurguzenkov@gmail.com>
2026-02-10 22:14:07 +0300

    Spike/use ce pkg (#4)
    * spike: use utility lib for dom element creation
    
    * 0.1.2
    
    * chore: update gh action
    
    * chore: add read package permission
    
    * chore: bump version

--- src/seed-form.js
+++ src/seed-form.js
@@ -1,48 +1,45 @@
+import $ from "@gooz92/ce";
+
 import { getDefaultSeed, SEED_PATTERN, formatSeed } from "./seed-utils";
 
 export const seedForm = (onSubmit) => {
-  const form = document.createElement('form');
-  form.noValidate = true;
 
   let initialSeed;
+
+  const input = $('input', {
+    name: 'seed',
+    pattern: SEED_PATTERN.source,
+    required: true,
+    oninput: () => {
+      submitButton.disabled = input.value === initialSeed || !input.validity.valid
+    }
+  });
 
   const handleSubmit = () => {
     initialSeed = input.value;
     onSubmit(input.value);
     submitButton.disabled = true;
-  }
+  };
 
-  form.onsubmit = e => {
-    e.preventDefault();
-    if (form.checkValidity()) {
+  const refreshButton = $('button', '↻', {
+    type: 'button',
+    onclick: () => {
+      input.value = formatSeed(getDefaultSeed());
       handleSubmit(input.value);
     }
-  };
+  });
 
-  const input = document.createElement('input');
-  input.name = 'seed';
-  input.pattern = SEED_PATTERN.source;
-  input.required = true;
+  const submitButton = $('button', 'OK', { disabled: true });
 
-  input.oninput = () => {
-    submitButton.disabled = input.value === initialSeed || !input.validity.valid
-  };
-
-  const refreshButton = document.createElement('button');
-  refreshButton.type = 'button';
-
-  refreshButton.onclick = () => {
-    input.value = formatSeed(getDefaultSeed());
-    handleSubmit(input.value);
-  };
-  
-  refreshButton.innerText = '↻';
-
-  const submitButton = document.createElement('button');
-  submitButton.innerText = 'OK';
-  submitButton.disabled = true;
-
-  form.append(input, refreshButton, submitButton);
+  const form = $('form', {
+    noValidate: true,
+    onsubmit: e => {
+      e.preventDefault();
+      if (form.checkValidity()) {
+        handleSubmit(input.value);
+      }
+    }
+  }, [ input, refreshButton, submitButton ]);
 
   return {
     element: form,
